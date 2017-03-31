aa4f5cf5dcea400d3e1c0d7cf7fe779b66f5910a
Raghu Betina <raghu@firstdraft.com>
2017-03-31 16:29:53 +0000

    Direct association between users and photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,8 @@
 class Photo < ApplicationRecord
   # Direct associations
 
+  belongs_to :user
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :photos,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
