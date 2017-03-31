428d72f4f052130903abdc4dfe1a2c84686b53b6
Raghu Betina <raghu@firstdraft.com>
2017-03-31 16:30:15 +0000

    Whitelist columns in FriendRequest for ActiveAdmin

--- app/admin/friend_request.rb
+++ app/admin/friend_request.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register FriendRequest do
 
+ permit_params :sender_id, :recipient_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
