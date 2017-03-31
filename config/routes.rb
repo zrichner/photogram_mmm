82c50afa581327b24694d790026dad89c0b0dcb6
Raghu Betina <raghu@firstdraft.com>
2017-03-31 16:30:02 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "photos#index"
   # Routes for the Friend_request resource:
   # CREATE
