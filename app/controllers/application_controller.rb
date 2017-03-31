3bc7db29a91688d14e0a77d7c80de98e57cd2c22
Raghu Betina <raghu@firstdraft.com>
2017-03-31 16:30:15 +0000

    Allow additional fields through security

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, :keys => [:user_name])
+
+    devise_parameter_sanitizer.permit(:account_update, :keys => [:user_name])
+  end
 end
